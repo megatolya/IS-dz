4afb3fd45825b93ae9bec23edfb739b587a2ea45
megatolya <forgetthisbox@gmail.com>
2012-11-06 11:36:36 +0400

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function showResult() {
         verSum +=ver;
         str += '<span class="result__number">'+k+' = '+v.code + ', вероятность: '+ver+'</span>';
     });
-    str +='Средняя длина = ' + verSum;
+    str +='<span class="result__number">Средняя длина = ' + verSum + '</span>';
     $('.result').html(str).fadeIn(300, function() {
         console.log('Готово');
         makeTree($('.tree'), max);
